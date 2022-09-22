ffc4eabd01dcec5b4e588712a21c3b662698c9e7
Sebastian Wieczorek <sebastian.wieczorek1989@gmail.com>
2022-09-22 23:20:53 +0200

    added updating and deleting dancer

--- registration/src/routes/Dancers.js
+++ registration/src/routes/Dancers.js
@@ -1,4 +1,11 @@
-import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
+} from "firebase/firestore";
 import React, { useEffect } from "react";
 import { useState } from "react";
 import { useFirebase } from "../contexts/FirebaseContext";
@@ -6,7 +13,11 @@ import { useFirebase } from "../contexts/FirebaseContext";
 function Dancers() {
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
+  const [editedFirstName, setEditedFirstName] = useState("");
+  const [editedLastName, setEditedLastName] = useState("");
   const [dancers, setDancers] = useState([]);
+  const [isEditDancerMode, setEditDancerMode] = useState(false);
+  const [editedDancerId, setEditDancerId] = useState("");
   const firebase = useFirebase();
 
   useEffect(() => {
@@ -36,6 +47,33 @@ function Dancers() {
     });
   };
 
+  const editDancer = (id) => {
+    setEditDancerMode(true);
+    setEditDancerId(id);
+  };
+
+  const updateDancer = (id) => {
+    const dancerDoc = doc(
+      firebase.firestore,
+      "users/" + firebase.user.uid + "/dancers",
+      id
+    );
+    updateDoc(dancerDoc, {
+      firstName: editedFirstName,
+      lastName: editedLastName,
+    });
+    setEditDancerMode(false);
+  };
+
+  const deleteDancer = (id) => {
+    const dancerDoc = doc(
+      firebase.firestore,
+      "users/" + firebase.user.uid + "/dancers",
+      id
+    );
+    deleteDoc(dancerDoc);
+  };
+
   return (
     <>
       <input
@@ -53,6 +91,23 @@ function Dancers() {
         {dancers.map((dancer) => (
           <li key={dancer.id}>
             {dancer.firstName} {dancer.lastName}
+            <button onClick={() => editDancer(dancer.id)}>Edytuj</button>
+            <button onClick={() => deleteDancer(dancer.id)}>Usuń</button>
+            {isEditDancerMode && editedDancerId === dancer.id && (
+              <div>
+                <input
+                  defaultValue={dancer.firstName}
+                  onChange={(e) => setEditedFirstName(e.target.value)}
+                ></input>
+                <input
+                  defaultValue={dancer.lastName}
+                  onChange={(e) => setEditedLastName(e.target.value)}
+                ></input>
+                <button onClick={() => updateDancer(dancer.id)}>
+                  Zapisz zmianę
+                </button>
+              </div>
+            )}
           </li>
         ))}
       </ul>
